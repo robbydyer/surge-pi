27337c2d3633ebd0565af791882af79292f5fcb5
rdyer <rdyer@rsglab.com>
2020-04-11 23:00:49 -0400

    wip

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Run this on the Raspberry-pi to start the container
 
+if ! dpkg -l | grep docker.io; then
+  apt-get update
+  apt-get install -y docker.io
+fi
+
 docker run -d \
   --privileged \
   --publish 80:80 \
