ee959c2f810a1fdd71b3a3dc88ee9c5ff45d0979
root <robby.dyer@gmail.com>
2020-04-16 13:48:02 +0100

    wip

--- run.sh
+++ run.sh
@@ -6,8 +6,22 @@ if ! dpkg -l | grep docker.io; then
   apt-get install -y docker.io
 fi
 
+if [ ! -f /etc/init.d/surge-pi ] || ! diff /etc/init.d/surge-pi surge.initd &> /dev/null; then
+  echo "=> Installing /etc/init.d/surge-pi"
+  cp surge.initd /etc/init.d/surge-pi
+  chmod 755 /etc/init.d/surge-pi
+fi
+
+c="surge-pi"
+if docker inspect "${c}" &> /dev/null; then
+  echo "=> Killing container ${c}"
+  docker kill "${c}"
+fi
+docker rm "${c}"
+
 docker run -d \
+  --name surge \
   --privileged \
-  --publish 80:80 \
+  --publish 8080:80 \
   surge-pi \
-  bash
+  bash -cex "while true; do sleep 6000; done"
